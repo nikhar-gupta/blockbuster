b6c3455c60bc3ff1daeb98684484b399845f00cc
Nikhar <nikhar.gupta99@gmail.com>
2025-06-15 15:48:45 +0530

    image path changed

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,14 +1,11 @@
 import "./search.css";
+import homelogo from "../../assets/full-logo.png";
+import heroImg from "../../assets/hero-img.png";
 const Search = ({ searchText, setSearchText }) => {
   return (
     <div className="searchMain">
-      <img
-        alt="logo"
-        className="homeLogo"
-        src="src\assets\full-logo.png"
-        width={500}
-      />
-      <img alt="cards" className="cardsImg" src="src\assets\hero-img.png" />
+      <img alt="logo" className="homeLogo" src={homelogo} width={500} />
+      <img alt="cards" className="cardsImg" src={heroImg} />
       <h1>
         Your <span>Movie</span> Universe
       </h1>
